eeb7cdf2bdfc3bf08ada476dea6d6c54ec1a6fba
danielp30 <daniel.p30@gso.schule.koeln>
2024-02-23 13:45:55 +0100

    Custom Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -13,7 +12,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['degularbold'],
+                body:['degular']
             },
         },
     },
