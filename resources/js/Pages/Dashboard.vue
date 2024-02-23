87d2bdf0261ae3cdf6715047df6c2c15f8c5827c
jboll1 <119185002+jboll1@users.noreply.github.com>
2024-02-23 14:05:55 +0100

    component tag in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,13 +2,36 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import Sidebar from "@/Components/Sidebar.vue";
+import Reports from "@/Components/Reports.vue";
+import Overview from "@/Components/Overview.vue";
+</script>
+<script>
+export default {
+    props: {
+       /* user: {
+            type: [Object],
+        },*/
+    },
+    data() {
+        return {
+            currentComponent: "overview",
+        };
+    },
+    components: {
+        Sidebar, Reports, Overview
+    },
+    computed: {},
+    methods: {},
+    mounted() {},
+    watch: {},
+};
 </script>
 
 <template>
     <div class="flex flex-row bg-[#FFF7F1]">
-        <sidebar></sidebar>
+        <sidebar :user="user" @reports="currentComponent = 'reports'" @overview="currentComponent = 'overview'"></sidebar>
         <div class="p-12 w-3/5 bg-white w-screen rounded-xl m-12">
-            <h1 class="text-2xl">Hallo Admin</h1>
+            <component :is="currentComponent"></component>
         </div>
     </div>
 </template>
