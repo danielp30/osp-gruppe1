2564b07fbdcc1129bdd113f5e6e4842c45fec181
jboll1 <119185002+jboll1@users.noreply.github.com>
2024-03-01 12:24:40 +0100

    fetch items in userOverview

--- resources/js/Components/UserOverview.vue
+++ resources/js/Components/UserOverview.vue
@@ -3,3 +3,28 @@
         UserOverview
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.fetchItems();
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get('/items?all=true');
+        this.items = response.data;
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    },
+  },
+};
+</script>
