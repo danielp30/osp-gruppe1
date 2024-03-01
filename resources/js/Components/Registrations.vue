f5c05bf79912c5c7f05c2edce998762c6430049d
Antonio Miraldi <antonio.miraldi@digitalbynature.de>
2024-03-01 13:56:52 +0100

    Registrations Overview

--- resources/js/Components/Registrations.vue
+++ resources/js/Components/Registrations.vue
@@ -1,5 +1,17 @@
+<script>
+import Table from "@/Components/RegistrationsTable.vue";
+import {defineComponent} from "vue";
+import RegistrationsTable from "@/Components/RegistrationsTable.vue";
+import DangerButton from "../../../vendor/laravel/breeze/stubs/inertia-vue-ts/resources/js/Components/DangerButton.vue";
+
+export default defineComponent({
+    components: {DangerButton, RegistrationsTable, Table}
+})
+
+</script>
 <template>
     <div>
-        Registrations
+        <h1 class="text-2xl pb-10">Anmeldungen</h1>
+        <RegistrationsTable></RegistrationsTable>
     </div>
 </template>
---
+++ resources/js/Components/RegistrationsTable.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { MoreHorizontal } from 'lucide-vue-next';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import DangerButton from "../../../vendor/laravel/breeze/stubs/inertia-vue-ts/resources/js/Components/DangerButton.vue";
+</script>
+<template>
+    <table class="border border-2 border-black w-full m-5 rounded-xl">
+        <thead class="border border-2 border-black">
+        <tr class="text-up">
+            <th class="px-6 py-3 text-left">Jahr</th>
+            <th class="px-6 py-3 text-left">Datum Freitag</th>
+            <th class="px-6 py-3 text-left">Datum Samtag</th>
+            <th class="px-6 py-3 text-left">Stand</th>
+            <th class="px-6 py-3 text-left"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="px-6 py-4 ">2024</td>
+            <th class="px-6 py-3 text-left">02-03-2024</th>
+            <th class="px-6 py-3 text-left">-</th>
+            <td class="px-6 py-4 ">C10</td>
+            <td class="px-6 py-4"><button>Anmelden</button></td>
+        </tr>
+        <tr>
+            <td class="px-6 py-4 ">2023</td>
+            <th class="px-6 py-3 text-left">04-03-2023</th>
+            <th class="px-6 py-3 text-left">-</th>
+            <td class="px-6 py-4 ">C10</td>
+            <td class="px-6 py-4 "><button>Anmelden</button></td>
+        </tr>
+        </tbody>
+    </table>
+</template>
