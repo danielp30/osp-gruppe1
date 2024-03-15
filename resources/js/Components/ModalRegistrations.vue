d8cbc60138ef07d18c4a6f0868365483ff3eb2f6
danielp30 <daniel.p30@gso.schule.koeln>
2024-03-15 11:58:50 +0100

    Style button

--- resources/js/Components/ModalRegistrations.vue
+++ resources/js/Components/ModalRegistrations.vue
@@ -41,8 +41,7 @@ const submitForm = async () => {
             <button
                 type="button"
                 @click="toggleModal"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                style="background: rgb(255, 212, 181); color: black;">
+                class="bg-[#FFD4B5] p-4 hover:bg-black hover:text-white transition-all">
                 Neue Anmeldung vornehmen
             </button>
         </div>
